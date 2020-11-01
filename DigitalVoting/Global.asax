95b3caf2fc4bd4f49c35e515ea21c5c863502254
George Anastasiadis <anastasiadisgeo87@gmail.com>
2020-11-01 11:53:15 +0200

    Add project files.

---
+++ DigitalVoting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DigitalVoting.MvcApplication" Language="C#" %>
---
+++ DigitalVoting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DigitalVoting.Startup))]
+namespace DigitalVoting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
