49c959f1da41f3567fbce643f6660a0dac6fdb08
George Anastasiadis <anastasiadisgeo87@gmail.com>
2020-11-14 20:51:59 +0200

    Creating Ballot Collection

---
+++ DigitalVoting/Controllers/BallotsController.cs
@@ -0,0 +1,26 @@
+﻿using DigitalVoting.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalVoting.Controllers
+{
+    public class BallotsController : Controller
+    {
+        IBallotData db;
+
+        public BallotsController()
+        {
+            db = new InMemoryBallotData();
+        }
+
+        public ActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+
+    }
+}
