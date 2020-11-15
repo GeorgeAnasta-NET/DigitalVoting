0c673c02141d9c37f1e764a1134a780fac56e566
George Anastasiadis <anastasiadisgeo87@gmail.com>
2020-11-15 20:10:23 +0200

    Create and Save a new Ballot

--- DigitalVoting/Controllers/BallotsController.cs
+++ DigitalVoting/Controllers/BallotsController.cs
@@ -36,42 +36,42 @@ namespace DigitalVoting.Controllers
             return View(viewModel);
         }
 
-        //public  ActionResult Save(BallotFormViewModel ballotModel)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        var balModel = new BallotFormViewModel()
-        //        {
-        //            BallotTypes = context.BallotTypes.ToList()
-        //        };
-        //        return View("Index", balModel);
-        //        //return View("BallotForm", balModel);
-        //    }
+        public ActionResult Save(BallotFormViewModel ballotModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var balModel = new BallotFormViewModel()
+                {
+                    BallotTypes = context.BallotTypes.ToList()
+                };
+                return View("Index", balModel);
+                //return View("BallotForm", balModel);
+            }
 
-        //    if (ballotModel.Id == 0)  // Create
-        //    {
-        //        var ballot = new Ballot()
-        //        {
-        //            Id = ballotModel.Id,
-        //            Name = ballotModel.Name,
-        //            TypeId = (int)ballotModel.BallotType
-        //        };
-        //        context.Ballots.Add(ballot);
-        //    }
-        //    //else  //Edit
-        //    //{
-        //    //    var vinylDb = context.Vinyls.SingleOrDefault(v => v.Id == vinylModel.Id);
-        //    //    // autes oi tesseris grammes mporeis na peis oti einai i whiteList
-        //    //    vinylDb.Title = vinylModel.Title;
-        //    //    vinylDb.Artist = vinylModel.Artist;
-        //    //    vinylDb.ReleaseYear = vinylModel.ReleaseYear;
-        //    //    vinylDb.GenreId = vinylModel.Genre;
-        //    //    vinylDb.LabelId = vinylModel.Label;
-        //    //    vinylDb.Price = vinylModel.Price;
-        //    //}
-        //    context.SaveChanges();
+            if (ballotModel.Id == 0)  // Create
+            {
+                var ballot = new Ballot()
+                {
+                    Id = ballotModel.Id,
+                    Name = ballotModel.Name,
+                    TypeId = (int)ballotModel.BallotType
+                };
+                context.Ballots.Add(ballot);
+            }
+            //else  //Edit
+            //{
+            //    var vinylDb = context.Vinyls.SingleOrDefault(v => v.Id == vinylModel.Id);
+            //    // autes oi tesseris grammes mporeis na peis oti einai i whiteList
+            //    vinylDb.Title = vinylModel.Title;
+            //    vinylDb.Artist = vinylModel.Artist;
+            //    vinylDb.ReleaseYear = vinylModel.ReleaseYear;
+            //    vinylDb.GenreId = vinylModel.Genre;
+            //    vinylDb.LabelId = vinylModel.Label;
+            //    vinylDb.Price = vinylModel.Price;
+            //}
+            context.SaveChanges();
 
-        //    return RedirectToAction("Index", "Ballots");
-        //}
+            return RedirectToAction("Index", "Ballots");
+        }
     }
 }
