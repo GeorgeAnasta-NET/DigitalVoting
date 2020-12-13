7fbd354573002a11bd789e456997ab9deaf7b53b
George Anastasiadis <anastasiadisgeo87@gmail.com>
2020-12-13 17:09:12 +0200

    Creating Combination Model

---
+++ DigitalVoting/Models/Combination.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigitalVoting.Models
+{
+    public class Combination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
