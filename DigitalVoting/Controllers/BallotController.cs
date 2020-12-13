2fc710a9a4356c6735f386675bd479c7da8cbbab
George Anastasiadis <anastasiadisgeo87@gmail.com>
2020-12-13 18:07:38 +0200

    Create BallotController

---
+++ DigitalVoting/Controllers/BallotController.cs
@@ -0,0 +1,25 @@
+﻿using DigitalVoting.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalVoting.Controllers
+{
+    public class BallotController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        //ctor
+        public BallotController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Ballot
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DigitalVoting/Models/Ballot.cs
+++ DigitalVoting/Models/Ballot.cs
@@ -9,5 +9,6 @@ namespace DigitalVoting.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<ApplicationUser> Candidates { get; set; }
     }
 }
